07f5478632a4db08dcb2d8f5ffd6a0770f04668f
dan <visan.dan.constantin@gmail.com>
2019-05-01 10:33:17 +0300

    Initial commit

---
+++ src/main/java/ro/vis/cercetare/inputadministrator/InputAdministratorApplication.java
@@ -0,0 +1,13 @@
+package ro.vis.cercetare.inputadministrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputAdministratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InputAdministratorApplication.class, args);
+	}
+
+}
